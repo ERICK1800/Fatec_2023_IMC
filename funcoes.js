af258b9f3b8d97c9f8ca8bb0a3f527d79d063359
ERICK1800 <ericklimaflorencio@gmail.com>
2023-03-07 10:42:51 -0300

    limpar + correções

--- funcoes.js
+++ funcoes.js
@@ -8,17 +8,23 @@ function CalcularIMC(){
 
     if (imc<18.5) {
         resp = 'Magreza!' + '\n' + 'Seu IMC é de :' + imc;
-    }else if (18.5<=imc && imc<=24.9) {
+    }else if (imc<25) {
         resp = 'Normal!' + '\n' + 'Seu IMC é de :' + imc;
-    }else if (25<=imc && imc<=29.9) {
+    }else if (imc<30) {
         resp = 'Sobrepeso!' + '\n' + 'Seu IMC é de :' + imc;
-    }else if (30<=imc && imc<=34.9) {
+    }else if (imc<35) {
         resp = 'Obesidade grau I!' + '\n' + 'Seu IMC é de :' + imc;
-    }else if (35<=imc && imc<=39.9) {
+    }else if (imc<40) {
         resp = 'Obesidade grau II!' + '\n' + 'Seu IMC é de :' + imc;
-    }else if (40<imc) {
+    }else{
         resp = 'Obesidade grau III!' + '\n' + 'Seu IMC é de :' + imc;
     }
 
     document.getElementById("resp").value = resp;
+}
+
+function limparCampos() {
+    document.getElementById("altura").value = "";
+    document.getElementById("peso").value = "";
+    document.getElementById("resp").value = "";
 }
