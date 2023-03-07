3e038641732df7d5d6ade86e60a152fc353c9d74
ERICK1800 <ericklimaflorencio@gmail.com>
2023-03-07 10:27:19 -0300

    cálculo

--- funcoes.js
+++ funcoes.js
@@ -1,12 +1,24 @@
-function Calcular(){
-    var sexo, idade, altura, peso, resp;
+function CalcularIMC(){
+    var altura, peso, resp, imc;
 
-    sexo = parseFloat(document.getElementById("sexo").value);
-    idade = parseFloat(document.getElementById("idade").value);
     altura = parseFloat(document.getElementById("altura").value);
     peso = parseFloat(document.getElementById("peso").value);
 
-    resp = peso / (altura*altura);
+    imc = peso / (altura*altura);
+
+    if (imc<18.5) {
+        resp = 'Magreza!' + '\n' + 'Seu IMC é de :' + imc;
+    }else if (18.5<=imc && imc<=24.9) {
+        resp = 'Normal!' + '\n' + 'Seu IMC é de :' + imc;
+    }else if (25<=imc && imc<=29.9) {
+        resp = 'Sobrepeso!' + '\n' + 'Seu IMC é de :' + imc;
+    }else if (30<=imc && imc<=34.9) {
+        resp = 'Obesidade grau I!' + '\n' + 'Seu IMC é de :' + imc;
+    }else if (35<=imc && imc<=39.9) {
+        resp = 'Obesidade grau II!' + '\n' + 'Seu IMC é de :' + imc;
+    }else if (40<imc) {
+        resp = 'Obesidade grau III!' + '\n' + 'Seu IMC é de :' + imc;
+    }
 
     document.getElementById("resp").value = resp;
 }
