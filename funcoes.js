7963936243aae57434eaeafbc574205919503127
ERICK1800 <ericklimaflorencio@gmail.com>
2023-03-07 11:02:42 -0300

    Style

--- funcoes.js
+++ funcoes.js
@@ -4,20 +4,20 @@ function CalcularIMC(){
     altura = parseFloat(document.getElementById("altura").value);
     peso = parseFloat(document.getElementById("peso").value);
 
-    imc = peso / (altura*altura);
+    imc = (peso / (altura*altura)).toFixed(2);
 
     if (imc<18.5) {
-        resp = 'Magreza!' + '\n' + 'Seu IMC é de :' + imc;
+        resp = 'Magreza!' + '\n' + 'Seu IMC é de:' + imc;
     }else if (imc<25) {
-        resp = 'Normal!' + '\n' + 'Seu IMC é de :' + imc;
+        resp = 'Normal!' + '\n' + 'Seu IMC é de:' + imc;
     }else if (imc<30) {
-        resp = 'Sobrepeso!' + '\n' + 'Seu IMC é de :' + imc;
+        resp = 'Sobrepeso!' + '\n' + 'Seu IMC é de:' + imc;
     }else if (imc<35) {
-        resp = 'Obesidade grau I!' + '\n' + 'Seu IMC é de :' + imc;
+        resp = 'Obesidade grau I!' + '\n' + 'Seu IMC é de:' + imc;
     }else if (imc<40) {
-        resp = 'Obesidade grau II!' + '\n' + 'Seu IMC é de :' + imc;
+        resp = 'Obesidade grau II!' + '\n' + 'Seu IMC é de:' + imc;
     }else{
-        resp = 'Obesidade grau III!' + '\n' + 'Seu IMC é de :' + imc;
+        resp = 'Obesidade grau III!' + '\n' + 'Seu IMC é de:' + imc;
     }
 
     document.getElementById("resp").value = resp;
